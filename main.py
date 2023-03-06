118f9b157580f3400dc26b970a53a8745507c0d0
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2023-03-06 20:04:29 +0200

    Finished the notebooks

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.vectorizers import chi2_select, tfidf
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('data/spam.csv')[['category', 'email']]
-    X, y = df['email'].values, df['category']
+    df = pd.read_csv('data/spam.csv')[['category', 'text']]
+    X, y = df['text'].values, df['category']
     print(f'TFIDF Accuracy: {round(base_test(tfidf(X, y), y), 5)}')
     print(f'Chi^2 Accuracy: {round(base_test(chi2_select(X, y), y), 5)}')
