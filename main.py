fef28032a2f33c8446518b6e746f19b1d80acecb
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2023-03-06 17:09:24 +0200

    minor fixes

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ from src.vectorizers import chi2_select, tfidf
 if __name__ == '__main__':
     df = pd.read_csv('data/spam.csv')[['v1', 'v2']]
     X, y = df['v2'].values, df['v1']
-    print(f'TFIDF Accuracy: {round(base_test(tfidf(X, y), y), 2)}')
-    print(f'Chi^2 Accuracy: {round(base_test(chi2_select(X, y), y), 2)}')
+    print(f'TFIDF Accuracy: {round(base_test(tfidf(X, y), y), 5)}')
+    print(f'Chi^2 Accuracy: {round(base_test(chi2_select(X, y), y), 5)}')
--- src/__init__.py
+++ src/__init__.py
@@ -6,12 +6,8 @@ from sklearn.naive_bayes import GaussianNB
 
 from src.kfold_cross_validation import validate
 
-def describe(df):
-    print(df.describe())
-
-
 def base_test(X, y):
-    describe(pd.DataFrame(X))
+    print(pd.DataFrame(X).describe())
     start = default_timer()
     res = validate(GaussianNB(), X, y, 100)
     print(f'Validation took {round(default_timer() - start, 2)} seconds')
