37d443485191b140e96d18a825a6c209a71cfe3a
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2023-03-16 13:11:55 +0200

    Naive Bayes implemented from scratch

--- main.py
+++ main.py
@@ -1,11 +1,54 @@
-
+import numpy as np
 import pandas as pd
-from src import base_test
-from src.vectorizers import chi2_select, tfidf
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, precision_score, recall_score
+from sklearn.model_selection import KFold
+
+from src.nb import NB
+
+
+def score_test(y_pred: np.ndarray, y_test, pos_label):
+    return {
+        'accuracy': round(accuracy_score(y_test, y_pred), 3),
+        'f1': round(f1_score(y_test, y_pred, pos_label=pos_label), 3),
+        'recall': round(recall_score(y_test, y_pred, average="binary", pos_label=pos_label), 3),
+        'precision': round(precision_score(y_test, y_pred, pos_label=pos_label), 3),
+    }
+
+
+def test_model_kfold(model, X, y, pos_label, verbose=False):
+    kf = KFold(n_splits=5, random_state=1, shuffle=True)
+    scores = {'accuracy': [], 'f1': [], 'recall': [], 'precision': []}
+    for train_index, test_index in kf.split(X):
+        X_train, X_test, y_train, y_test = (
+            X.iloc[train_index],
+            X.iloc[test_index],
+            y.iloc[train_index],
+            y.iloc[test_index]
+        )
+        model.fit(X_train, y_train)
+        y_pred = [model.predict(row) for index, row in X_test.iterrows()]
+        results = score_test(y_pred, y_test, pos_label)
+        scores = {k: scores[k] + [results[k]] for k in scores}
+    return scores
+
+
+def print_scores(scores):
+    print('Accuracy:', '%.3f +/- %.3f' %
+          (np.mean(scores['accuracy']), np.std(scores['accuracy'])))
+    print('F1 Score:', '%.3f +/- %.3f' %
+          (np.mean(scores['f1']), np.std(scores['f1'])))
+    print('Recall:', '%.3f +/- %.3f' %
+          (np.mean(scores['recall']), np.std(scores['recall'])))
+    print('Precision:', '%.3f +/- %.3f' %
+          (np.mean(scores['precision']), np.std(scores['precision'])))
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('data/spam.csv', encoding='ISO 8859-15')[['category', 'text']]
-    X, y = df['text'].values, df['category']
-    print(f'TFIDF Accuracy: {round(base_test(tfidf(X, y), y), 5)}')
-    print(f'Chi^2 Accuracy: {round(base_test(chi2_select(X, y), y), 5)}')
+    df = pd.read_csv('data/tennis.csv')
+    label = 'Play'
+    X, y = df.loc[:, df.columns != label], df[label]
+    # X = X.astype(np.float32)
+    # X = (df-df.mean())/df.std()
+    # y = y.map({'M': 1, 'B': 0})
+    scores = test_model_kfold(NB(), X, y, 'Yes')
+    print_scores(scores)
--- src/nb/__init__.py
+++ src/nb/__init__.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+
 class ModelNotFittedException(Exception):
     def __init__(self):
         super().__init__('Model not fitted')
@@ -16,7 +17,7 @@ class NB(object):
     def _cp(x, n, k, alpha):
         return (x + alpha)/(n + k*alpha)
 
-    def _cond_prob(self, df: pd.DataFrame, col: str, target: str) -> dict[tuple[str, str], float]:
+    def _cond_prob(self, X: pd.DataFrame, y, col: str, target: str) -> dict[tuple[str, str], float]:
         """Calculates conditional probability for a dataframe column with respect to a target column
 
         Args:
@@ -28,18 +29,18 @@ class NB(object):
             float: The conditional probability of occurrence for each value in the column.
         """
 
-        def calc(g):
-            return self._cp(g[g[target] == l].shape[0], g.shape[0], len(self.labels), self.alpha)
+        def calc(intersect, all_vals):
+            return self._cp(intersect, all_vals, len(self.labels), self.alpha)
         prob = {}
-        for l in self.labels:
-            for v, g in df.groupby(col):
-                prob[(v, l)] = calc(g)
+        label_indexes = {l: y.index[y == l].tolist() for l in self.labels}
+        for label, index_list in label_indexes.items():
+            for v, g in X.groupby(col):
+                prob[(v, label)] = calc(g.iloc[g.index.isin(index_list)].shape[0], g.shape[0])
         return prob
 
     def fit(self, X: pd.DataFrame, y):
         self.labels = y.unique()
-        self.prob = {col: self._cond_prob(
-            pd.concat([X, y], axis=1), col, y.name) for col in X.columns}
+        self.prob = {col: self._cond_prob(X, y, col, y.name) for col in X.columns}
 
     def predict(self, X):
         if self.prob is None or self.labels is None:
@@ -51,7 +52,6 @@ class NB(object):
                 res *= self.prob[col].get((val, lvalue), )
             return res
         label_probs = {l: p(X.to_dict(), l) for l in self.labels}
-        print(label_probs)
         return max(label_probs, key=label_probs.get)
 
 
@@ -70,7 +70,7 @@ class GaussianNB(NB):
             self.priors[c] = X_c.shape[0] / X.shape[0]
             self.means[c] = np.mean(X_c, axis=0)
             self.covs[c] = np.diag(np.diag(np.cov(X_c, rowvar=False)))
-            
+
     def predict(self, X):
         if self.classes is None:
             raise ModelNotFittedException()
@@ -82,7 +82,8 @@ class GaussianNB(NB):
                 inv_cov = np.linalg.inv(self.covs[c])
                 inv_cov_det = np.linalg.det(inv_cov)
                 diff = x-self.means[c]
-                likelihood = 0.5*np.log(inv_cov_det) - 0.5*diff.T @ inv_cov @ diff
+                likelihood = 0.5*np.log(inv_cov_det) - \
+                    0.5*diff.T @ inv_cov @ diff
                 post = prior + likelihood
                 posts.append(post)
             pred = self.classes[np.argmax(posts)]
